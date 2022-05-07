e235a6de4705e5d4eaee0dff7f60b2173afbad37
itedya <kontakt@itedya.com>
2022-05-07 23:19:37 +0200

    Changed app port and added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SerialportModule } from "./serialport/serialport.module";
 import { ArduinoModule } from "./arduino/arduino.module";
+import {ConfigModule} from "./config/config.module";
 
 @Module({
-  imports: [ArduinoModule, SerialportModule],
+  imports: [ConfigModule, ArduinoModule, SerialportModule],
 })
 export class AppModule {}
