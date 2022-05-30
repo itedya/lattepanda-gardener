a48e8e2735af33f7d429c5dc2bc45c35c2b51301
itedya <kontakt@itedya.com>
2022-05-30 21:34:15 +0200

    Added Database module import

--- src/arduino-configuration/arduino-configuration.module.ts
+++ src/arduino-configuration/arduino-configuration.module.ts
@@ -1,8 +1,10 @@
 import {Module} from "@nestjs/common";
 import {ArduinoConfigurationController} from "./arduino-configuration.controller";
 import {ArduinoConfigurationService} from "./arduino-configuration.service";
+import {DatabaseModule} from "../database/database.module";
 
 @Module({
+    imports: [DatabaseModule],
     controllers: [ArduinoConfigurationController],
     providers: [ArduinoConfigurationService],
     exports: [ArduinoConfigurationService]
