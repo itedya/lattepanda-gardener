809e19ec5e5ebb16a726f3d45db4ac27150134d3
itedya <kontakt@itedya.com>
2022-05-30 18:54:59 +0200

    Added create func in Arduino Configuration

--- src/arduino-configuration/arduino-configuration.controller.ts
+++ src/arduino-configuration/arduino-configuration.controller.ts
@@ -1,6 +1,15 @@
-import {Controller} from "@nestjs/common";
+import {Body, Controller, Post} from "@nestjs/common";
+import {CreateArduinoConfigurationDto} from "./dtos/create-arduino-configuration.dto";
+import {ArduinoConfigurationService} from "./arduino-configuration.service";
+import {ArduinoConfigurationDto} from "./dtos/arduino-configuration.dto";
 
 @Controller("/api/arduino-configurations")
 export class ArduinoConfigurationController {
+    constructor(private arduinoConfigurationService: ArduinoConfigurationService) {
+    }
 
+    @Post("/")
+    async create(@Body() createDto: CreateArduinoConfigurationDto): Promise<ArduinoConfigurationDto> {
+        return this.arduinoConfigurationService.create(createDto);
+    }
 }
--- src/arduino-configuration/arduino-configuration.service.ts
+++ src/arduino-configuration/arduino-configuration.service.ts
@@ -1,6 +1,23 @@
+import {v4 as uuidv4} from 'uuid';
 import {Injectable} from "@nestjs/common";
+import {DatabaseService} from "../database/database.service";
+import {CreateArduinoConfigurationDto} from "./dtos/create-arduino-configuration.dto";
+import {ArduinoConfigurationDto} from "./dtos/arduino-configuration.dto";
+import {classToPlain} from "class-transformer";
 
 @Injectable()
 export class ArduinoConfigurationService {
+    constructor(private databaseService: DatabaseService) {
+    }
 
+    async create(createDto: CreateArduinoConfigurationDto): Promise<ArduinoConfigurationDto> {
+        const db = this.databaseService.getDatabase();
+
+        const dto = new ArduinoConfigurationDto(classToPlain(createDto));
+
+        db.data.arduinoConfigurations.push(dto);
+        await db.write();
+
+        return dto;
+    }
 }
