e9013e48099bdda99a7a2fab0eb15b5e6db5522a
itedya <kontakt@itedya.com>
2022-05-31 10:17:27 +0200

    Added validation constraints in create-arduino-configuration.dto.ts

--- src/arduino-configuration/dtos/create-arduino-configuration.dto.ts
+++ src/arduino-configuration/dtos/create-arduino-configuration.dto.ts
@@ -1,7 +1,24 @@
 import {SerialportDto} from "./serialport.dto";
+import {ArrayMaxSize, IsArray, IsNotEmpty, IsString, Length, Validate} from "class-validator";
+import {IsValidSerialport} from "../../serialport/validators/is-valid-serialport.validator";
+import {IsArduinoConfigurationNameUnique} from "../validators/is-arduino-configuration-name-unique.validator";
+import {IsArduinoConfigurationPathUnique} from "../validators/is-arduino-configuration-path-unique.validator";
 
 export class CreateArduinoConfigurationDto {
+    @IsNotEmpty()
+    @IsString()
+    @Length(3, 64)
+    @Validate(IsArduinoConfigurationNameUnique)
     name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Validate(IsValidSerialport)
+    @Validate(IsArduinoConfigurationPathUnique)
     path: string;
+
+    @IsNotEmpty()
+    @IsArray()
+    @ArrayMaxSize(40)
     serialports: SerialportDto[];
 }
