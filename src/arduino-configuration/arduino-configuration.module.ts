731b7f4d0e955f391a23f10fdf3c91e54d1146ed
itedya <kontakt@itedya.com>
2022-05-31 10:20:56 +0200

    Added new validator constraints to arduino-configuration.module.ts

--- src/arduino-configuration/arduino-configuration.module.ts
+++ src/arduino-configuration/arduino-configuration.module.ts
@@ -2,11 +2,17 @@ import {Module} from "@nestjs/common";
 import {ArduinoConfigurationController} from "./arduino-configuration.controller";
 import {ArduinoConfigurationService} from "./arduino-configuration.service";
 import {DatabaseModule} from "../database/database.module";
+import {IsArduinoConfigurationNameUnique} from "./validators/is-arduino-configuration-name-unique.validator";
+import {IsArduinoConfigurationPathUnique} from "./validators/is-arduino-configuration-path-unique.validator";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [ArduinoConfigurationController],
-    providers: [ArduinoConfigurationService],
+    providers: [
+        IsArduinoConfigurationNameUnique,
+        IsArduinoConfigurationPathUnique,
+        ArduinoConfigurationService,
+    ],
     exports: [ArduinoConfigurationService]
 })
 export class ArduinoConfigurationModule {
