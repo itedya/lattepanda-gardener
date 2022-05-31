d22002b1695546bc6de0f57565c880f9ba5540f8
itedya <kontakt@itedya.com>
2022-05-31 10:15:34 +0200

    Made validator constraints inject services from app in constructor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
+import {useContainer} from "class-validator";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  // make validator constraints inject services in constructor
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(80);
 }
 bootstrap();
